5f782bb178bff64821b4feef093e88ace7c467b9
divyanshuan <vermadivyanshu000@gmail.com>
2023-02-22 15:58:04 +0530

    initial commit

---
+++ src/Components/battleground/battleground.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import data from "./battle.json";
+import { Button } from "@mui/material";
+import swal from "sweetalert";
+
+const Battleground = () => {
+  const fields = {
+    variant: "outlined",
+    fullWidth: true,
+  };
+  const [values, setValues] = useState({ name: "", roll: "", game: "" });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { name, roll, game } = values;
+    const res = await fetch(
+      "https://advik-cb2ad-default-rtdb.firebaseio.com/Battleground.json",
+      {
+        method: "POST",
+        Headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name,
+          roll,
+          game,
+        }),
+      }
+    );
+    if (res.status === 200) {
+      swal("Submitted", "good", "success");
+      setValues({ name: "", roll: "", game: "" });
+    } else {
+      swal("Error Happeend", "Try Again", "error");
+    }
+    console.log(res);
+  };
+  return (
+    <div>
+      <h1>BattleGround Registration</h1>
+      <Grid
+        container
+        pt={4}
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={3}>
+          <TextField
+            name="name"
+            label="Name"
+            {...fields}
+            onChange={handleChange}
+            value={values.name}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            name="roll"
+            label="Roll No"
+            {...fields}
+            onChange={handleChange}
+            value={values.roll}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <FormControl fullWidth>
+            <InputLabel id="funzone">Game</InputLabel>
+            <Select
+              labelId="funzone"
+              name="game"
+              label="Game"
+              value={values.game}
+              onChange={handleChange}
+            >
+              {data.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={2}>
+          <Button variant="outlined" onClick={handleSubmit}>
+            submit
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Battleground;
---
+++ src/Components/event/event.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import InputLabel from "@mui/material/InputLabel";
+import FormControl from "@mui/material/FormControl";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import data from "./event.json";
+import { Button } from "@mui/material";
+import logo from "../../assets/logo.png";
+import swal from "sweetalert";
+const Event = () => {
+  const fields = {
+    variant: "outlined",
+    fullWidth: true,
+  };
+  const [values, setValues] = useState({ name: "", roll: "", event: "" });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({ ...values, [name]: value });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const { name, roll, event } = values;
+    const res = await fetch(
+      "https://advik-cb2ad-default-rtdb.firebaseio.com/Event.json",
+      {
+        method: "POST",
+        Headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          name,
+          roll,
+          event,
+        }),
+      }
+    );
+    if (res.status === 200) {
+      swal("Submitted", "good", "success");
+      setValues({ name: "", roll: "", event: "" });
+    } else {
+      swal("Error Happeend", "Try Again", "error");
+    }
+  };
+  return (
+    <div>
+      <img width={200} src={logo} alt="" />
+
+      <h1>Event Registration</h1>
+      <Grid
+        container
+        pt={4}
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={3}>
+          <TextField
+            name="name"
+            label="Name"
+            {...fields}
+            onChange={handleChange}
+            value={values.name}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <TextField
+            name="roll"
+            label="Roll No"
+            {...fields}
+            onChange={handleChange}
+            value={values.roll}
+          />
+        </Grid>
+        <Grid item xs={2}>
+          <FormControl fullWidth>
+            <InputLabel id="funzone">Event</InputLabel>
+            <Select
+              labelId="funzone"
+              name="event"
+              label="Event"
+              value={values.event}
+              onChange={handleChange}
+            >
+              {data.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.name}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={2}>
+          <Button variant="outlined" onClick={handleSubmit}>
+            submit
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Event;
